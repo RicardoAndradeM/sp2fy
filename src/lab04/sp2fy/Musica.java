08ecdf8294f2ffe75a3a693b7da14575511b8a09
Ricardo Andrade <ricardo.maia12@gmail.com>
2016-07-19 14:25:11 -0300

    commit inicial

---
+++ src/lab04/sp2fy/Musica.java
@@ -0,0 +1,15 @@
+package lab04.sp2fy;
+
+public class Musica {
+
+	private String titulo;
+	private int duracao;
+	private String genero;
+
+	public Musica(String titulo, int duracao, String genero) {
+		this.titulo = titulo;
+		this.duracao = duracao;
+		this.genero = genero;
+	}
+
+}
